54f5babf650b96bc51e07c1bb441f11c361d1a8a
PassiveLemon <lemonl3mn@protonmail.com>
2024-01-30 15:59:13 -0500

    Update default.nix

--- default.nix
+++ default.nix
@@ -10,7 +10,6 @@
 
 {
   # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 }
