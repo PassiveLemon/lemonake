b38ee9359e4009e3425d5abe88cb1861ff03e616
PassiveLemon <lemonl3mn@protonmail.com>
2024-01-30 16:42:05 -0500

    Refactor: completely ditch the NUR design
    Move pulsemeeter out because it never worked. Why ship an incomplete package?

--- flake.nix
+++ flake.nix
@@ -2,28 +2,21 @@
   description = "Lemon's Flakes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    systems = [
-      "x86_64-linux"
-      "i686-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "armv6l-linux"
-      "armv7l-linux"
+  outputs = { self, ... } @ inputs:
+  inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    flake.nixosModules = {
+      alvr = import ./modules/alvr.nix;
+    };
+    imports = [
+      ./pkgs
     ];
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-  in
-  {
-    legacyPackages = forAllSystems (system: import ./default.nix {
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-      };
-    });
-    packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
   };
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,25 @@
-{ pkgs ? import <nixpkgs> { } }: 
+{ inputs, self, ... }: {
+  imports = [inputs.flake-parts.flakeModules.easyOverlay];
 
-with pkgs; rec {
-  alvr = callPackage ./alvr { };
-  animdl = python3Packages.callPackage ./animdl { };
-  gdlauncher = callPackage ./gdlauncher { };
-  gdlauncher-carbon = callPackage ./gdlauncher-carbon { };
-  gfm = callPackage ./gfm { };
-  poepyautopot = python3Packages.callPackage ./poepyautopot { };
-  pulsemeeter = python3Packages.callPackage ./pulsemeeter { };
-  tilp2 = callPackage ./tilp2 { inherit gfm; };
-  xclicker = callPackage ./xclicker { };
+  systems = [ "x86_64-linux" ];
+
+  perSystem = { config, system, pkgs, ... }: {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
+    overlayAttrs = config.packages;
+
+    packages = with pkgs; rec {
+      alvr = callPackage ./alvr { };
+      animdl = python3Packages.callPackage ./animdl { };
+      gdlauncher = callPackage ./gdlauncher { };
+      gdlauncher-carbon = callPackage ./gdlauncher-carbon { };
+      gfm = callPackage ./gfm { };
+      poepyautopot = python3Packages.callPackage ./poepyautopot { };
+      tilp2 = callPackage ./tilp2 { inherit gfm; };
+      xclicker = callPackage ./xclicker { };
+    };
+  };
 }
