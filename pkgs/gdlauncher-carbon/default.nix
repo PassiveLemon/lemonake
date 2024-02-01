d9a0066a9b6c96fba3a7bf1f403351d48c29ec23
PassiveLemon <lemonl3mn@protonmail.com>
2024-02-01 16:30:04 -0500

    Update: GDLauncher-Carbon

--- pkgs/gdlauncher-carbon/default.nix
+++ pkgs/gdlauncher-carbon/default.nix
@@ -4,10 +4,10 @@
 }:
 let
   pname = "gdlauncher-carbon";
-  version = "beta.1705798181";
+  version = "beta.1706720580";
   src = fetchurl {
     url = "https://cdn-raw.gdl.gg/launcher/GDLauncher__2.0.0-${version}__linux__x64.AppImage";
-    hash = "sha256-5XznjDk0ea9NVff1/fpRDgtvLPRDDQIaeY5yQXxSHv0=";
+    hash = "sha256-W2h1VTxb/qU14TrfF4LBs9x7yfsR1TNYcUiaJmeUPsU=";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
