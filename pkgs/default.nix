62bf30e34b6e47df61298090725f752d44e5222a
PassiveLemon <lemonl3mn@protonmail.com>
2024-03-02 14:11:20 -0500

    Remove: xclicker as its merged in nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -20,7 +20,10 @@
       hd2pystratmacro = python3Packages.callPackage ./hd2pystratmacro { };
       poepyautopot = python3Packages.callPackage ./poepyautopot { };
       tilp2 = callPackage ./tilp2 { inherit gfm; };
-      xclicker = callPackage ./xclicker { };
     };
   };
 }
+
+# Manually update:
+# nix run /home/lemon/Documents/GitHub/lemonake#packages.x86_64-linux.<package>
+# Until I set up NVFetcher or something to automatically do this.
