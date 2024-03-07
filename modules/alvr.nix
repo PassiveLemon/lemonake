53b929c9d1200dcc6f418df03dd237fa2a9e964e
PassiveLemon <lemonl3mn@protonmail.com>
2024-03-07 17:55:43 -0500

    Refactor: ALVR module

--- modules/alvr.nix
+++ modules/alvr.nix
@@ -1,21 +1,19 @@
 { config, pkgs, lib, ... }:
 
-with lib;
-
 let
   cfg = config.programs.alvr;
+  inherit (lib) mkIf mkEnableOption mkPackageOption mkOption mdDoc types maintainers;
 in
 {
   options = {
     programs.alvr = {
-      enable = mkEnableOption (lib.mdDoc "Whether to enable ALVR, the VR desktop streamer.");
+      enable = mkEnableOption "Whether to enable ALVR, the VR desktop streamer";
 
       package = mkPackageOption pkgs "alvr" { };
 
       openFirewall = mkOption {
         type = types.bool;
-        default = false;
-        description = lib.mdDoc ''
+        description = mdDoc ''
           Whether to open the default ports in the firewall for the ALVR server.
         '';
       };
@@ -30,6 +28,5 @@ in
       allowedUDPPorts = [ 9943 9944 ];
     };
   };
-
   meta.maintainers = with maintainers; [ passivelemon ];
 }
