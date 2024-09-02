57a3342f65bd706fbd25cb5d2f3ec14cf302f8e0
PassiveLemon <lemonl3mn@protonmail.com>
2024-09-02 00:04:47 -0400

    Chore: Cleanup

--- pkgs/default.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, config, lib, ... }: {
-
-}
-
-# Manually update:
-# nix run /home/lemon/Documents/GitHub/lemonake#packages.x86_64-linux.<package>
-# Until I set up NVFetcher or something to automatically do this.
-
