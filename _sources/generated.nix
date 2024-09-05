46f97fe3ee6f91d6c9af7475b06101ed8f711c14
PassiveLemon <lemonl3mn@protonmail.com>
2024-09-05 18:02:14 -0400

    Chore: Update sources

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -32,6 +32,22 @@
     };
     date = "2023-10-17";
   };
+  gdlauncher-carbon = {
+    pname = "gdlauncher-carbon";
+    version = "2.0.20";
+    src = fetchurl {
+      url = "https://cdn-raw.gdl.gg/launcher/GDLauncher__2.0.20__linux__x64.AppImage";
+      sha256 = "sha256-tI9RU8qO3MHbImOGw2Wl1dksNbhqrYFyGemqms8aAio=";
+    };
+  };
+  gdlauncher-legacy = {
+    pname = "gdlauncher-legacy";
+    version = "v1.1.30";
+    src = fetchurl {
+      url = "https://github.com/gorilla-devs/GDLauncher/releases/download/v1.1.30/GDLauncher-linux-setup.AppImage";
+      sha256 = "sha256-4cXT3exhoMAK6gW3Cpx1L7cm9Xm0FK912gGcRyLYPwM=";
+    };
+  };
   hd2pystratmacro = {
     pname = "hd2pystratmacro";
     version = "1.0.2";
@@ -64,7 +80,6 @@
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
-      sparseCheckout = [ ];
       sha256 = "sha256-QabYVKcenW+LQ+QSjUoQOLOQAVHdjE0YXd+1WsdzNPc=";
     };
     date = "2024-02-21";
@@ -92,6 +107,29 @@
     };
     date = "2024-05-18";
   };
+  tilp2-gfm = {
+    pname = "tilp2-gfm";
+    version = "1.18";
+    src = fetchFromGitHub {
+      owner = "debrouxl";
+      repo = "tilp_and_gfm";
+      rev = "1.18";
+      fetchSubmodules = false;
+      sha256 = "sha256-/XkxEfWzJiOkM5aoenp/GQSkkNg9qoXkFtcj/nenFEw=";
+    };
+  };
+  tilp2-gfm-git = {
+    pname = "tilp2-gfm-git";
+    version = "5a2ea6337c4983b80375a50e3ada1f7c45f6e9ea";
+    src = fetchFromGitHub {
+      owner = "debrouxl";
+      repo = "tilp_and_gfm";
+      rev = "5a2ea6337c4983b80375a50e3ada1f7c45f6e9ea";
+      fetchSubmodules = false;
+      sha256 = "sha256-sIwio294jvb21N0XTFhDGbd1uq/yFfgrxBRtMw+NVew=";
+    };
+    date = "2023-11-12";
+  };
   wivrn = {
     pname = "wivrn";
     version = "v0.18";
@@ -105,14 +143,38 @@
   };
   wivrn-git = {
     pname = "wivrn-git";
-    version = "a4522d2ad7569013ab612e736cbc56e10afecf23";
+    version = "2141c66c6eb5c2135c955d2fcdb4e800fc7c93d6";
     src = fetchFromGitHub {
       owner = "WiVRn";
       repo = "WiVRn";
-      rev = "a4522d2ad7569013ab612e736cbc56e10afecf23";
+      rev = "2141c66c6eb5c2135c955d2fcdb4e800fc7c93d6";
       fetchSubmodules = false;
-      sha256 = "sha256-OUBZ4ujRUHrfPBt4UtpRt76XOZU+AiEjXiz+YaiNKZQ=";
+      sha256 = "sha256-ZkOoDD6TsqMYXihye015DJ3e7ZuThVzPcGJUQrysG2A=";
     };
-    date = "2024-09-04";
+    date = "2024-09-05";
+  };
+  wivrn-git-monado = {
+    pname = "wivrn-git-monado";
+    version = "2d3978b1b0d0f1ce9fc20f435c7080a07124362a";
+    src = fetchgit {
+      url = "https://gitlab.freedesktop.org/monado/monado.git";
+      rev = "2d3978b1b0d0f1ce9fc20f435c7080a07124362a";
+      fetchSubmodules = false;
+      deepClone = false;
+      leaveDotGit = false;
+      sha256 = "sha256-1qnokZYCA4h88eQmTpKYnHczeLh19Pjw2fM6y9rzn/Q=";
+    };
+  };
+  wivrn-monado = {
+    pname = "wivrn-monado";
+    version = "dfc602288ab05131584a3f2be18031a13fccd061";
+    src = fetchgit {
+      url = "https://gitlab.freedesktop.org/monado/monado.git";
+      rev = "dfc602288ab05131584a3f2be18031a13fccd061";
+      fetchSubmodules = false;
+      deepClone = false;
+      leaveDotGit = false;
+      sha256 = "sha256-4HZs3cgqOWWpXQb5kfG513f7+znO0hJvAbj2rxrqmeI=";
+    };
   };
 }
