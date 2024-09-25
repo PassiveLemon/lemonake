59c656ab30b3e8c1bf64833f01124937a89ac52f
PassiveLemon <lemonl3mn@protonmail.com>
2024-09-25 17:01:16 -0400

    Fix: Picom tag and git

--- overlays/default.nix
+++ overlays/default.nix
@@ -72,12 +72,28 @@ in
       picom-tag = let
         package = getPackage "picom-tag" prev;
       in
-      prev.picom.overrideAttrs { inherit (package) version src; };
+      prev.picom.overrideAttrs (prevAttrs: {
+        inherit (package) version src;
+
+        nativeBuildInputs = (final.lib.subtractLists [
+          final.asciidoc
+        ] prevAttrs.nativeBuildInputs) ++ [
+          final.asciidoctor
+        ];
+      });
 
       picom-git = let
         package = getPackage "picom-git" prev;
       in
-      prev.picom.overrideAttrs { inherit (package) version src; };
+      prev.picom.overrideAttrs (prevAttrs: {
+        inherit (package) version src;
+
+        nativeBuildInputs = (final.lib.subtractLists [
+          final.asciidoc
+        ] prevAttrs.nativeBuildInputs) ++ [
+          final.asciidoctor
+        ];
+      });
 
       poepyautopot = let
         package = getPackage "poepyautopot" prev;
