5293dbbd831ea28e5df3431fe0c907074e565508
PassiveLemon <72527881+PassiveLemon@users.noreply.github.com>
2025-07-21 03:30:44 +0000

    chore: update flake and sources

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -405,20 +405,20 @@
   };
   xrizer-git = {
     pname = "xrizer-git";
-    version = "70271e02892eb08509c072067daa5983320b83ce";
+    version = "2fbce11c592a72db0a22ca97337d58d67f385bfa";
     src = fetchFromGitHub {
       owner = "Supreeeme";
       repo = "xrizer";
-      rev = "70271e02892eb08509c072067daa5983320b83ce";
+      rev = "2fbce11c592a72db0a22ca97337d58d67f385bfa";
       fetchSubmodules = false;
-      sha256 = "sha256-FWV/eRm8MRLTAel7648tHubsJFI0r2IJjBHJI0lvzI8=";
+      sha256 = "sha256-nQx0CKfwemfRkfNdVSuhQLx2hvuZ6G/WX/MU2/bzQx8=";
     };
     cargoLock."Cargo.lock" = {
-      lockFile = ./xrizer-git-70271e02892eb08509c072067daa5983320b83ce/Cargo.lock;
+      lockFile = ./xrizer-git-2fbce11c592a72db0a22ca97337d58d67f385bfa/Cargo.lock;
       outputHashes = {
         "openxr-0.19.0" = "sha256-mljVBbQTq/k7zd/WcE1Sd3gibaJiZ+t7td964clWHd8=";
       };
     };
-    date = "2025-07-18";
+    date = "2025-07-20";
   };
 }
