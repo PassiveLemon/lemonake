a177ad69be3dfcc807cdbcdc0d702db9913a774e
PassiveLemon <lemonl3mn@protonmail.com>
2025-11-09 14:19:41 -0500

    chore: wivrn 25.11

--- pkgs/wivrn/default.nix
+++ pkgs/wivrn/default.nix
@@ -9,19 +9,10 @@ in
         package = getPackage "wivrn" prev;
         monado = getPackage "wivrn-monado" prev;
       in
-      (prev.callPackage ./package.nix {
+      prev.callPackage ./package.nix {
         inherit (package) src;
         version = versionFromPackage package;
         monadoSrc = monado.src;
-      }).overrideAttrs {
-        patches = [
-          # See https://github.com/WiVRn/WiVRn/pull/557
-          (prev.fetchpatch {
-            name = "wivrn-fix-qt6.10-build.patch";
-            url = "https://github.com/WiVRn/WiVRn/commit/2204fdd39682cfc052556d58fdb9404dd8ecf63f.patch?full_index=1";
-            hash = "sha256-05MLfJNCznBt6eaggUfSk1jaNDB2/eou6CfexUkIHZE=";
-          })
-        ];
       };
 
       wivrn-git = let
