3bc735d7d61c4eb72e3d3c5560321a0da7cd2e66
PassiveLemon <lemonl3mn@protonmail.com>
2025-12-23 18:56:21 -0500

    feature: somewm release

--- pkgs/somewm/default.nix
+++ pkgs/somewm/default.nix
@@ -5,9 +5,7 @@ in
 {
   flake.overlays = {
     somewm = final: prev: {
-      # The current (as of 12/20/25) somewm stable release does not build in nix
-      #somewm = packager "somewm" ./package.nix prev { };
-      somewm = packager "somewm-git" ./package.nix prev { };
+      somewm = packager "somewm" ./package.nix prev { };
 
       somewm-git = packager "somewm-git" ./package.nix prev { };
     };
