ecd54f8f5c93eef8bde82aced33ef0f6f795ea6b
PassiveLemon <lemonl3mn@protonmail.com>
2026-05-11 23:37:24 -0400

    remove: deprecation notice

--- modules/nixos/wivrn/default.nix
+++
@@ -1,8 +0,0 @@
-{ lib, ... }: {
-  imports = [
-    (lib.mkRemovedOptionModule [ "services" "wivrn" ] ''
-      This module has been identical to the nixpkgs wivrn module for a long time, use that instead.
-    '')
-  ];
-}
-
