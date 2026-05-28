a17813ef22b265c41eb04a1ce4c821e33d7082ba
PassiveLemon <lemonl3mn@protonmail.com>
2026-05-28 17:40:16 -0400

    feature: somewm hm module

--- docs/somewm.md
+++ docs/somewm.md
@@ -1,52 +1,36 @@
 # SomeWM
 [SomeWM GitHub](https://github.com/trip-zip/somewm)
 
-This repository features two modules for managing SomeWM, a NixOS module that enables UWSM and adds the SomeWM entry, and a home-manager module that sets up systemd services if UWSM is not enabled (home-manager module is currently not complete).
+This repository features two modules for managing SomeWM, a NixOS module that enables UWSM and adds the SomeWM entry, and a home-manager module (Incomplete) that sets up systemd services if UWSM is not enabled.
 
-By default, SomeWM will check for your AwesomeWM config at `~/.config/awesome` if it doesn't find one at `~/.config/somewm`. This means that all you need to do is expose any extra libraries to the SomeWM package and it should work nearly flawlessly. If you aren't using the modules in this repository, you can simply append a `--search <path-to-lib>` for each extra library. If you are using the modules in this repository however, you need to override the package to search them. The examples below are shown for the home-manager module but the same package option exists for the NixOS module.
+By default, SomeWM will check for your AwesomeWM config at `~/.config/awesome` if it doesn't find one at `~/.config/somewm`. This means that all you need to do is expose any extra libraries to the SomeWM package and it should work nearly flawlessly, but do note the [deviations](https://github.com/trip-zip/somewm/blob/main/DEVIATIONS.md) as they may cause issues.
 
 > [!NOTE]
 > SomeWM uses LuaJIT by default, there may be unexpected conflicts when using libraries built on non-JIT Lua.
 
-To expose extra lua libraries from nixpkgs, you can override the package and add them to extraLuaModules:
+To expose extra lua libraries from nixpkgs, you can add them to `wayland.windowManager.somewm.extraLuaModules`
 ```nix
 # home.nix
 {
   wayland.windowManager.somewm = {
     enable = true;
-    package = (inputs.lemonake.packages.${pkgs.system}.somewm-git.override {
-      extraLuaModules = with pkgs.luajitPackages; [
-        luafilesystem
-      ];
-    };
+    package = inputs.lemonake.packages.${pkgs.system}.somewm;
+    extraLuaModules = with pkgs.luajitPackages [ luafilesystem ];
   };
 }
 ```
 
-For custom libraries and/or shared objects, you can add those to extraSearchPaths:
-```nix
-# home.nix
-{
-  wayland.windowManager.somewm = {
-    enable = true;
-    package = (inputs.lemonake.packages.${pkgs.system}.somewm-git.override {
-      extraSearchPaths = [
-        inputs.lemonake.packages.${pkgs.system}.lua-pam-luajit-git
-      ];
-    };
-  };
-}
-```
+For custom libraries and/or shared objects, you can add those to extraSearchPaths. The structure is the exact same as `extraLuaModules`.
 
-For custom GI type library paths, you can add those to extraGITypeLibPaths:
+For custom GI type library paths, you can add those to extraGITypeLibPaths. This is only possible through a package override so a module option does not exist:
 ```nix
 # home.nix
 {
   wayland.windowManager.somewm = {
     enable = true;
-    package = (inputs.lemonake.packages.${pkgs.system}.somewm-git.override {
-      extraGITypeLibPaths = with pkgs; [
-        networkmanager upower
+    package = (inputs.lemonake.packages.${pkgs.system}.somewm.override {
+      extraGITypeLibPaths = with pkgs.luajitPackages; [
+        networkmanager upower playerctl # These are already packaged with SomeWM
       ];
     };
   };
--- modules/home-manager/somewm/default.nix
+++ modules/home-manager/somewm/default.nix
@@ -1,12 +1,17 @@
 { config, pkgs, lib, ... }:
 let
-  inherit (lib) mkIf mkEnableOption mkPackageOption mkOption types maintainers;
+  inherit (lib) mkIf mkEnableOption mkPackageOption mkOption types maintainers concatMapStrings concatStringsSep;
   cfg = config.wayland.windowManager.somewm;
-  systemdModule = {
-    options = {
-      # TODO
-    };
-  };
+
+  # variables = concatStringsSep " " cfg.systemd.variables;
+  # extraCommands = concatStringsSep " " (map (f: "&& ${f}") cfg.systemd.extraCommands);
+  # systemdActivation = "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd ${variables} ${extraCommands}";
+  
+  getLuaPath = lib: dir: "${lib}/${dir}/lua/${cfg.lua.luaversion}";
+  makeSearchPaths = concatMapStrings (
+    path: "--search ${getLuaPath path "share"}" + "--search ${getLuaPath path "lib"}"
+  );
+  finalSearchPaths = makeSearchPaths ([ cfg.extraLuaModules ] ++ cfg.extraSearchPaths);
 in
 {
   options = {
@@ -15,8 +20,60 @@ in
 
       package = mkPackageOption pkgs "somewm" { };
 
-      systemd = mkOption {
-        type = types.submodule systemdModule;
+      lua = mkPackageOption pkgs "luajit" { };
+
+      systemd = {
+        # TODO: Finish. References: https://github.com/nix-community/home-manager/blob/release-25.11/modules/services/window-managers/river.nix
+        enable = mkEnableOption null // {
+          default = true;
+          description = ''
+            Whether to enable {file}`somewm-session.target` on
+            somewm startup. This links to {file}`graphical-session.target`}.
+            Some important environment variables will be imported to systemd
+            and D-Bus user environment before reaching the target, including
+            - `DISPLAY`
+            - `WAYLAND_DISPLAY`
+            - `XDG_CURRENT_DESKTOP`
+          '';
+        };
+
+        useService = mkOption {
+          default = false;
+          type = types.bool;
+          description = "use a systemd service rather than a target.";
+        };
+        
+        variables = mkOption {
+          type = types.listOf types.str;
+          default = [
+            "DISPLAY"
+            "WAYLAND_DISPLAY"
+            "XDG_CURRENT_DESKTOP"
+          ];
+          example = [ "-all" ];
+          description = "Environment variables to be imported in the systemd & D-Bus user environment.";
+        };
+
+        extraCommands = mkOption {
+          type = types.listOf types.str;
+          default = [
+            "systemctl --user stop somewm-session.target"
+            "systemctl --user start somewm-session.target"
+          ];
+          description = "Extra commands to be run after D-Bus activation.";
+        };
+      };
+
+      extraLuaModules = mkOption {
+        type = types.listOf types.package;
+        description = "Extra Lua modules to add to the SomeWM search path";
+        default = [ ];
+      };
+
+      extraSearchPaths = mkOption {
+        type = types.listOf types.package;
+        description = "Other extra search paths to add to SomeWM";
+        default = [ ];
       };
     };
   };
@@ -33,6 +90,32 @@ in
         xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
       ];
+    };
+
+    systemd.user.targets.somewm-session = lib.mkIf cfg.systemd.enable {
+      Unit = {
+        Description = "SomeWM compositor session";
+        Documentation = [ "man:systemd.special(7)" ];
+        BindsTo = [ "graphical-session.target" ];
+        Wants = [ "graphical-session-pre.target" ];
+        After = [ "graphical-session-pre.target" ];
+      };
+    };
+
+    systemd.user.services.somewm = lib.mkIf cfg.systemd.useService {
+      Unit = {
+        Description = "SomeWM Wayland compositor";
+        BindsTo = [ "graphical-session.target" ];
+        Wants = [ "graphical-session-pre.target" ];
+        After = [ "graphical-session-pre.target" ];
+        X-SwitchMethod = "reload";
+      };
+      Service = {
+        Slice = [ "session.slice" ];
+        Type = "notify";
+        ExecStart = "${cfg.package}/bin/somewm ${finalSearchPaths}";
+        ExecReload = "${cfg.package}/bin/somewm-client reload";
+      };
     };
   };
   meta.maintainers = with maintainers; [ passivelemon ];
