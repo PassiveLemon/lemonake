f7387f3c95beb79b3d7773dcb749179fd6e7465d
PassiveLemon <lemonl3mn@protonmail.com>
2026-06-02 02:00:06 -0400

    refactor: group CI package sets
    The idea is to create the package sets in the flake so that doesn't need to be done in workflow files. This is more versatile

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,6 @@
 { self, inputs, config, lib, pkgs, ... }:
 let
+  inherit (lib) map foldl';
   libLemonake = import ../parts/lib.nix { inherit config lib pkgs; };
 
   overlayPackages = [
@@ -23,9 +24,23 @@ let
     "xrizer"
   ];
 
-  overlayPaths = builtins.map (name: (libLemonake.subImport ./${name})) overlayPackages;
+  overlayPaths = map (name: (libLemonake.subImport ./${name})) overlayPackages;
 in
 {
+  imports = [
+    ./ci.nix
+  ] ++ overlayPaths;
+
+  flake.overlays = {
+    default = self.overlays.linux;
+
+    linux = final: prev:
+      let
+        overlayFns = map (name: self.overlays.${name}) overlayPackages;
+      in
+        foldl' (acc: f: acc // (f final prev)) { } overlayFns;
+  };
+
   perSystem = { system, ... }:
   let
     systemPkgs = import inputs.nixpkgs {
@@ -33,41 +48,9 @@ in
       config.allowUnfree = true;
       overlays = [ self.overlays.default ];
     };
-    systemLib = systemPkgs.lib;
-
-    inherit (systemLib) isList all attrNames filterAttrs;
-
-    flakePkgs = self.packages.${system};
-
-    testLicense = license:
-      license ? redistributable &&
-      license.redistributable;
-
-    isRedistributable = pkg:
-      pkg.meta ? license &&
-      (if isList pkg.meta.license
-      then all testLicense pkg.meta.license
-      else testLicense pkg.meta.license);
-
-    redistributablePkgs = attrNames (filterAttrs (_: pkg: isRedistributable pkg) flakePkgs);
-    nonRedistributablePkgs = attrNames (filterAttrs (_: pkg: ! isRedistributable pkg) flakePkgs);
   in
   {
     packages = self.overlays.default systemPkgs systemPkgs;
-    redistributablePackages = redistributablePkgs;
-    nonRedistributablePackages = nonRedistributablePkgs;
-  };
-
-  imports = overlayPaths;
-
-  flake.overlays = {
-    default = self.overlays.linux;
-
-    linux = final: prev:
-      let
-        overlayFns = builtins.map (name: self.overlays.${name}) overlayPackages;
-      in
-        builtins.foldl' (acc: f: acc // (f final prev)) { } overlayFns;
   };
 }
 
