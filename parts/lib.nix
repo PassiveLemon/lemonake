15d599226c3ee359cbafc505ca54a52528a3602b
PassiveLemon <lemonl3mn@protonmail.com>
2026-06-05 12:40:45 -0400

    chore: lib comments

--- parts/lib.nix
+++ parts/lib.nix
@@ -3,25 +3,26 @@ let
   inherit (lib) extend removePrefix;
 in
 extend (final: _: {
+# Shortcut to import modules with the custom lib
   subImport = path: import path {
     inherit config;
     lib = final;
   };
 
+  # Get packages from nvfetcher
   getPackage = pname: pkgs: (pkgs.callPackage ../_sources/generated.nix { }).${pname};
 
-  versionRemovePrefix = version:
-    removePrefix "v" version;
+  # Version formatting
+  versionRemovePrefix = version: removePrefix "v" version;
+  versionGitDateToUnstable = date: "0-unstable-${date}";
 
-  versionGitDateToUnstable = date:
-    "0-unstable-${date}";
-
-  versionFromPackage = pkg: (
+  # Get the appropriately formatted version from an nvfetcher source
+  versionFromPackage = pkg:
     if pkg ? "date"
     then final.versionGitDateToUnstable pkg.date
-    else final.versionRemovePrefix pkg.version
-  );
+    else final.versionRemovePrefix pkg.version;
 
+  # Call a package with a source from nvfetcher
   packager = pname: path: pkgs:
     let
       pkg = final.getPackage pname pkgs;
@@ -30,6 +31,7 @@ extend (final: _: {
     in
     pkgs.callPackage path { inherit version src; };
 
+  # Override a package with a source from nvfetcher
   overlayPackager = pname: overridePkg: pkgs:
     let
       pkg = final.getPackage pname pkgs;
