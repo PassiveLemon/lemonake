24f420a6bf38cfcc1ab4086b8fe12c111ba8c1e5
PassiveLemon <lemonl3mn@protonmail.com>
2026-06-30 15:18:49 -0400

    fix: hilbish-git

--- pkgs/hilbish/default.nix
+++ pkgs/hilbish/default.nix
@@ -6,7 +6,7 @@ in
   flake.overlays = {
     hilbish = final: prev: {
       hilbish-git = (overlayPackager "hilbish-git" "hilbish" prev).overrideAttrs {
-        vendorHash = "sha256-dx4uNK2hpGEhBP9QKuuchcbWF2wMGTxQ6zPkC6+RFjE=";
+        vendorHash = "sha256-cbcob4b5pqaC/KbzXhFtLF5gsm9Ky364T98xZRlVQP8=";
       };
       # Hilbish with C-lua instead of Golua
       hilbish-midnight = (overlayPackager "hilbish-midnight" "hilbish" prev).overrideAttrs {
