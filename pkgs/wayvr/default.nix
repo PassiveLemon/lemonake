83e0652084f244d65ca6162f32b6b6418f91f515
PassiveLemon <lemonl3mn@protonmail.com>
2026-07-06 14:17:13 -0400

    fix: wayvr

--- pkgs/wayvr/default.nix
+++ pkgs/wayvr/default.nix
@@ -8,16 +8,20 @@ in
       wayvr = let
         package = getPackage "wayvr" prev;
       in
-      prev.wayvr.overrideAttrs {
+      prev.wayvr.overrideAttrs (prevAttrs: {
         inherit (package) src;
         version = versionFromPackage package;
         cargoDeps = final.rustPlatform.importCargoLock package.cargoLock."Cargo.lock";
+
+        buildInputs = prevAttrs.buildInputs ++ [
+          prev.dav1d
+        ];
 
         postInstall = ''
           install -Dm644 $src/wayvr/wayvr.desktop $out/share/applications/wayvr.desktop
           install -Dm644 $src/wayvr/wayvr.svg $out/share/icons/hicolor/scalable/apps/wayvr.svg
         '';
-      };
+      });
 
       wayvr-git = let
         package = getPackage "wayvr-git" prev;
